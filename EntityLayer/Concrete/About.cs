a526b1a02956d7567532fd724dabc42a4f62017c
yusufsw <yusuf-talha@outlook.com>
2025-12-05 01:33:28 +0300

    try just comment

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -12,5 +12,6 @@ namespace EntityLayer.Concrete
         public string Details { get; set; }
         public string Image { get; set; }
         public bool Status { get; set; }
+        //vscode
     }
 }
