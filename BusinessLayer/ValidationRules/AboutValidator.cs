4fdd1b64c6b30086a44396f4c431df5de8ca50ef
yusufsw <yusuf-talha@outlook.com>
2025-12-08 06:23:51 +0300

    Added Fluent Validation package and added template

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Aciklama kismini bos gecemezsiniz.");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lutfen gorsel seciniz");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lutfen en az 50 karakterlik aciklama giriniz");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Lutfen en fazla 1500 karakterlik aciklama giriniz");
+        }
+    }
+}
